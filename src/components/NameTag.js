41fa1272617371efad9f64406afcff33da0b71f5
Caleb Roberts <joesevario@gmail.com>
2023-02-15 23:44:19 -0600

    adjusting some code with nametag, will remove at device.xs

--- src/components/NameTag.js
+++ src/components/NameTag.js
@@ -4,32 +4,14 @@ import styled from 'styled-components'
 import { device } from '../worker/breakpoints'
 
 const Avatar = styled.img`
-  display: flex;
   border-radius: 50%;
   position: absolute;
   margin: 0;
   padding: 0;
   left: 1em;
   top: 0.5em;
-  width: 5em;
-  height: 5em;
-  margin-top: 0;
-  @media only screen and ${device.xs} {
-    width: 3em;
-    height: 3em;
-  }
-  @media only screen and ${device.sm} {
-    width: 4em;
-    height: 4em;
-  }
-  @media only screen and ${device.md} {
-    width: 4em;
-    height: 4em;
-  }
-  @media only screen and ${device.lg} {
-    width: 4em;
-    height: 4em;
-  }
+  width: 4em;
+  height: 4em;
 `
 const User = styled.h1`
   position: absolute;
@@ -39,25 +21,12 @@ const User = styled.h1`
   font-weight: 600;
   font-size: 200%;
   display: flex;
-  right: 15px;
-  top: 1vh;
   color: #ffffff;
+  font-size: 200%;
+    top: 1vh;
+    right: 15px;
   &:hover {
     cursor: context-menu;
-  }
-  @media only screen and ${device.xs} {
-    right: 10px;
-    font-size: 200%;
-    top: 1.5vh;
-  }
-  @media only screen and ${device.sm} {
-    font-size: 200%;
-  }
-  @media only screen and ${device.md} {
-    font-size: 200%;
-  }
-  @media only screen and ${device.lg} {
-    font-size: 200%;
   }
 `
 const Nametag = styled.div`
@@ -67,14 +36,11 @@ const Nametag = styled.div`
   max-height: 0vh;
   flex-direction: row;
   justify-content: space-between;
-  flex-grow: 0;
-  right: 7vw;
+  flex-grow: 1;
   top: 12vh;
-  padding: 5em;
-  padding-bottom: 0;
   border-radius: 8px;
   @media only screen and ${device.xs} {
-    right: 6vw;
+    display: none;
   }
   @media only screen and ${device.sm} {
     width: 4em;
@@ -82,6 +48,7 @@ const Nametag = styled.div`
     padding: 5em;
     padding-bottom: 0;
     right: 4vw;
+    display: flex;
   }
   @media only screen and ${device.md} {
     width: 4em;
@@ -89,6 +56,7 @@ const Nametag = styled.div`
     padding: 5em;
     padding-bottom: 0;
     right: 4vw;
+    display: flex;
   }
   @media only screen and ${device.lg} {
     width: 4em;
@@ -96,6 +64,7 @@ const Nametag = styled.div`
     padding: 5em;
     padding-bottom: 0;
     right: 5vw;
+    display: flex;
   }
   @media only screen and ${device.xlg} {
     width: 4em;
@@ -103,6 +72,7 @@ const Nametag = styled.div`
     padding: 5em;
     padding-bottom: 0;
     right: 5vw;
+    display: flex;
   }
 `
 
